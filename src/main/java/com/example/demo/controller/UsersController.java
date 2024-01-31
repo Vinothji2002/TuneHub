e4f7e5db4bb79f2507bcc169be7a2aaa67e58f4a
VINOTHJI V B <116940880+Vinothji2002@users.noreply.github.com>
2024-01-31 22:45:16 +0530

    UsersController.java

--- src/main/java/com/example/demo/controller/UsersController.java
+++ src/main/java/com/example/demo/controller/UsersController.java
@@ -18,47 +18,46 @@ import jakarta.servlet.http.HttpSession;
 public class UsersController {
 	@Autowired
 	UsersService service;
+	@Autowired
+	SongService songService;
 	@PostMapping("/registration")
 	public String addUsers(@ModelAttribute Users user) {
 		boolean userStatus=service.emailExists(user.getEmail());
 		if(userStatus==false) {
 			service.addUser(user);
 			System.out.println("user aadded");
-		}
+		} 
 		else
 			System.out.println("user already exist");
 		return "home";
 	}
 	
 	@PostMapping("/validate")
-	public String validate(@RequestParam("email") String email,@RequestParam("password") String password,HttpSession session) {
+	public String validate(@ModelAttribute Users users,HttpSession session,Model model) {
+		String email=users.getEmail();
+		String password=users.getPassword();
 		if(service.validateUser(email,password)==true) {
+			
 			String role=service.getRole(email);
 			session.setAttribute("email",email);	
 			if(role.equals("admin"))
 				return "adminHome";
-			else
+			else {
+				Users user=service.getUser(email);
+				boolean userStatus=user.isPremium();
+				List<Song> songsList = songService.fetchAllSongs();
+				model.addAttribute("songs", songsList);
+				model.addAttribute("isPremium", userStatus);
+				System.out.println(userStatus);
 				return "customerHome";
+			}
+			
 		}
 		else
 			return "login";
 	}
-/*	@GetMapping("/pay")
-	public String pay(@RequestParam String email) {
-		boolean paymentStatus=false;
-		//payment api
-		if(paymentStatus==true) {
-			Users user=service.getUser(email);
-			user.setPassword(true);
-			service.updateUser(user);
-		}
-		
-		return "login";
-	}
-*/
 	@GetMapping("/logout")
 	public String logout(HttpSession session) {
 		session.invalidate();
 		return "login";
 	}
-}
